6c1aae6e5eb0a0e96cf169fbef039b1c47f515a8
Troywxx <wangxx0214@163.com>
2019-07-11 16:03:34 +0800

    creat flask app

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import DevConfig
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+@app.route('/')
+def home():
+    return '<h>Hello World!</h>'
+
+if __name__ == '__main__':
+    app.run()
+
